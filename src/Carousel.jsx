2aa0ac564dc72157352aaca10447c90e73cb8444
Jared <jared_glenn@yahoo.com>
2024-02-29 20:49:05 -0700

    finish exercise

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -16,33 +16,53 @@ import Card from "./Card";
  */
  function Carousel({ photos, title }) {
   const [currCardIdx, setCurrCardIdx] = useState(0);
+  const [canGoLeft, setCanGoLeft] = useState(false);
+  const [canGoRight, setCanGoRight] = useState(true);
 
   const currCard = photos[currCardIdx];
   const total = photos.length;
 
   //Increments currCardIdx state by 1
   function goForward() {
-    setCurrCardIdx(currCardIdx + 1);
+    const nextIndex = currCardIdx + 1
+    setCurrCardIdx(nextIndex);
+
+    //Always can go left if moving forward.
+    setCanGoLeft(true)
+
+    // Can go right if next index is less than the total.
+    setCanGoRight(nextIndex < total - 1);
+  }
+
+  function goBack() {
+    const nextIndex = currCardIdx - 1
+    setCurrCardIdx(nextIndex);
+
+    //Can go left if next index is greater than 0.
+    setCanGoLeft(nextIndex > 0)
+
+    // Can always go right if moving backward.
+    setCanGoRight(true);
   }
 
   return (
     <div className="Carousel">
       <h1>{title}</h1>
       <div className="Carousel-main">
-        <i
+        {canGoLeft ? <i
           className="bi bi-arrow-left-circle"
-          onClick={goForward}
-        />
+          onClick={goBack}
+        /> : <i></i>}
         <Card
           caption={currCard.caption}
           src={currCard.src}
           currNum={currCardIdx + 1}
           totalNum={total}
         />
-        <i
+        {canGoRight ? <i
           className="bi bi-arrow-right-circle"
           onClick={goForward}
-        />
+        /> : <i></i>}
       </div>
     </div>
   );
--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -19,7 +19,7 @@ it("matches snapshot", () => {
   expect(asFragment()).toMatchSnapshot();
 });
 
-
+// Right Arrow Works
 it("works when you click on the right arrow", function() {
   const { container } = render(
     <Carousel
@@ -45,5 +45,88 @@ it("works when you click on the right arrow", function() {
   ).not.toBeInTheDocument();
   expect(
     container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+});
+
+// Left Arrow Works
+it("works when you click on the left arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+   // expect the second image to show, but not the first
+   expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+});
+
+
+// Right arrow disappears when at the last index.
+it("removes the right arrow when there are no more pictures", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // move forward in the carousel twice
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  // expect the right arrow to disappear and the left arrow to still be there
+  expect(
+    container.querySelector('i[class="bi bi-arrow-right-circle"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('i[class="bi bi-arrow-left-circle"]')
+  ).toBeInTheDocument();
+});
+
+// Left arrow disappears when at the first index.
+it("removes the left arrow when there are no more pictures", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // expect the left arrow to disappear and the right arrow to still be there
+  expect(
+    container.querySelector('i[class="bi bi-arrow-left-circle"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('i[class="bi bi-arrow-right-circle"]')
   ).toBeInTheDocument();
 });
